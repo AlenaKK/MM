229b5af390ab395a19b6522101097c1aa5d7bb52
AlenaKK <94434069+AlenaKK@users.noreply.github.com>
2021-11-23 10:35:45 +0500

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 void Privet() {
 	cout << "Privet krasotka!" << endl;
+	cout << " I have a problem" << endl;
 }
 
 four4angel::four4angel(void)
@@ -15,7 +16,7 @@ four4angel::four4angel(void)
 		p[i].y = 0.0;
 	}
 }
-void four4angel::calcVectors() // ôóíêöèÿ ñ÷èòàþùàÿ è âûâîäÿùàÿ âåêòîðà 
+void four4angel::calcVectors() // Ã´Ã³Ã­ÃªÃ¶Ã¨Ã¿ Ã±Ã·Ã¨Ã²Ã Ã¾Ã¹Ã Ã¿ Ã¨ Ã¢Ã»Ã¢Ã®Ã¤Ã¿Ã¹Ã Ã¿ Ã¢Ã¥ÃªÃ²Ã®Ã°Ã  
 {
 	p[NUM_POINTS].x = p[0].x;
 	p[NUM_POINTS].y = p[0].y;
@@ -23,7 +24,7 @@ void four4angel::calcVectors() // ôóíêöèÿ ñ÷èòàþùàÿ è âûâîäÿùàÿ âåêòîðà
 	{
 		v[i].x = p[i + 1].x - p[i].x;
 		v[i].y = p[i + 1].y - p[i].y;
-		cout << "Êîîðäèíàòû " << i+1 << " âåêòîðà " << v[i].x << '\t' << v[i].y << endl;
+		cout << "ÃŠÃ®Ã®Ã°Ã¤Ã¨Ã­Ã Ã²Ã» " << i+1 << " Ã¢Ã¥ÃªÃ²Ã®Ã°Ã  " << v[i].x << '\t' << v[i].y << endl;
 	}
 	d1.x = p[2].x - p[0].x;
 	d1.y = p[2].y - p[0].y;
@@ -58,26 +59,26 @@ four4angel::four4angel(vect* _v)
 }
 four4angel::four4angel(double _s1, double _s2, double _s3, double _s4, double _u)
 {
-	_u = _u * Pi / 180; // ïåðåâîäèì â ðàäèàíû
+	_u = _u * Pi / 180; // Ã¯Ã¥Ã°Ã¥Ã¢Ã®Ã¤Ã¨Ã¬ Ã¢ Ã°Ã Ã¤Ã¨Ã Ã­Ã»
 	p[0].x = 0.0;
 	p[0].y = 0.0;
 	p[1].x = _s1;
 	p[1].y = 0.0;
 	p[3].x = _s4 * round(cos(_u));
 	p[3].y = _s4 * round(sin(_u));
-	double Diagonal_naprotiv_ugla = sqrt(pow(_s4, 2) + pow(_s1, 2) - 2 *_s1*_s4* cos(_u)); // ïî òåîðåìå êîñèíóñîâ
-	double Ugol_4_1 = acos((pow(_s4, 2) - pow(_s1, 2) - pow(Diagonal_naprotiv_ugla, 2)) / ((-2) * Diagonal_naprotiv_ugla * _s1)); // óãîë ïîä äèàãîíàëüþ ñâåðõó
-	double Ugol_4_2= acos((pow(_s3, 2) - pow(_s2, 2) - pow(Diagonal_naprotiv_ugla, 2)) / ((-2) * _s2 * Diagonal_naprotiv_ugla)); // óãîë íàä äèàãîíàëüþ ñâåðõó
+	double Diagonal_naprotiv_ugla = sqrt(pow(_s4, 2) + pow(_s1, 2) - 2 *_s1*_s4* cos(_u)); // Ã¯Ã® Ã²Ã¥Ã®Ã°Ã¥Ã¬Ã¥ ÃªÃ®Ã±Ã¨Ã­Ã³Ã±Ã®Ã¢
+	double Ugol_4_1 = acos((pow(_s4, 2) - pow(_s1, 2) - pow(Diagonal_naprotiv_ugla, 2)) / ((-2) * Diagonal_naprotiv_ugla * _s1)); // Ã³Ã£Ã®Ã« Ã¯Ã®Ã¤ Ã¤Ã¨Ã Ã£Ã®Ã­Ã Ã«Ã¼Ã¾ Ã±Ã¢Ã¥Ã°ÃµÃ³
+	double Ugol_4_2= acos((pow(_s3, 2) - pow(_s2, 2) - pow(Diagonal_naprotiv_ugla, 2)) / ((-2) * _s2 * Diagonal_naprotiv_ugla)); // Ã³Ã£Ã®Ã« Ã­Ã Ã¤ Ã¤Ã¨Ã Ã£Ã®Ã­Ã Ã«Ã¼Ã¾ Ã±Ã¢Ã¥Ã°ÃµÃ³
 	double Ugol_4 = Ugol_4_1 + Ugol_4_2; 
-	double Diagonal_iz_ugla = sqrt(pow(_s1, 2) + pow(_s2, 2) - 2 * _s1 * _s2 * cos(Ugol_4)); // ïî òåîðåìå êîñèíóñîâ
-	double Ugol_1_1 = asin((_s2 * sin(Ugol_4)) / (Diagonal_iz_ugla)); // ÷àñòü èçâåñòíîãî óãëà ïîä äèàãîíàëüþ, âûõîäÿùåé èç íåãî
+	double Diagonal_iz_ugla = sqrt(pow(_s1, 2) + pow(_s2, 2) - 2 * _s1 * _s2 * cos(Ugol_4)); // Ã¯Ã® Ã²Ã¥Ã®Ã°Ã¥Ã¬Ã¥ ÃªÃ®Ã±Ã¨Ã­Ã³Ã±Ã®Ã¢
+	double Ugol_1_1 = asin((_s2 * sin(Ugol_4)) / (Diagonal_iz_ugla)); // Ã·Ã Ã±Ã²Ã¼ Ã¨Ã§Ã¢Ã¥Ã±Ã²Ã­Ã®Ã£Ã® Ã³Ã£Ã«Ã  Ã¯Ã®Ã¤ Ã¤Ã¨Ã Ã£Ã®Ã­Ã Ã«Ã¼Ã¾, Ã¢Ã»ÃµÃ®Ã¤Ã¿Ã¹Ã¥Ã© Ã¨Ã§ Ã­Ã¥Ã£Ã®
 
 	p[2].x = Diagonal_iz_ugla * cos(Ugol_1_1);
 	p[2].y = Diagonal_iz_ugla * sin(Ugol_1_1);
 
 	calcVectors();
 }
-void four4angel::calcVectors3d() // ôóíêöèÿ ñ÷èòàþùàÿ è âûâîäÿùàÿ âåêòîðà 
+void four4angel::calcVectors3d() // Ã´Ã³Ã­ÃªÃ¶Ã¨Ã¿ Ã±Ã·Ã¨Ã²Ã Ã¾Ã¹Ã Ã¿ Ã¨ Ã¢Ã»Ã¢Ã®Ã¤Ã¿Ã¹Ã Ã¿ Ã¢Ã¥ÃªÃ²Ã®Ã°Ã  
 {
 	p[NUM_POINTS].x = p[0].x;
 	p[NUM_POINTS].y = p[0].y;
@@ -89,15 +90,15 @@ void four4angel::calcVectors3d() // ôóíêöèÿ ñ÷èòàþùàÿ è âûâîäÿùàÿ âåêòîðà
 		v2[i].x = 0.0;
 		v2[i].y = 0.0;
 		v2[i].z = h;
-		cout << "Êîîðäèíàòû " << i + 1 << " âåêòîðà " << v[i].x << '\t' << v[i].y << '\t' << v[i].z << endl;
+		cout << "ÃŠÃ®Ã®Ã°Ã¤Ã¨Ã­Ã Ã²Ã» " << i + 1 << " Ã¢Ã¥ÃªÃ²Ã®Ã°Ã  " << v[i].x << '\t' << v[i].y << '\t' << v[i].z << endl;
 	}
 	for (int i = 0; i < NUM_POINTS;i++)
 	{
-		cout << "Êîîðäèíàòû " << i + 1 + 4 << " âåêòîðà " << v2[i].x << '\t' << v2[i].y << '\t' << v2[i].z << endl;
+		cout << "ÃŠÃ®Ã®Ã°Ã¤Ã¨Ã­Ã Ã²Ã» " << i + 1 + 4 << " Ã¢Ã¥ÃªÃ²Ã®Ã°Ã  " << v2[i].x << '\t' << v2[i].y << '\t' << v2[i].z << endl;
 	}
 	for (int i = 0; i < NUM_POINTS;i++)
 	{
-		cout << "Êîîðäèíàòû " << i + 1 + 8 << " âåêòîðà " << v[i].x << '\t' << v[i].y << '\t' << v[i].z << endl;
+		cout << "ÃŠÃ®Ã®Ã°Ã¤Ã¨Ã­Ã Ã²Ã» " << i + 1 + 8 << " Ã¢Ã¥ÃªÃ²Ã®Ã°Ã  " << v[i].x << '\t' << v[i].y << '\t' << v[i].z << endl;
 	}
 	d1.x = p[2].x - p[0].x;
 	d1.y = p[2].y - p[0].y;
@@ -144,7 +145,7 @@ four4angel::four4angel(vect* _v, double _h)
 four4angel::four4angel(double _s1, double _s2, double _s3, double _s4, double _u, double _h)
 {
 	h = _h;
-	_u = _u * Pi / 180; // ïåðåâîäèì â ðàäèàíû
+	_u = _u * Pi / 180; // Ã¯Ã¥Ã°Ã¥Ã¢Ã®Ã¤Ã¨Ã¬ Ã¢ Ã°Ã Ã¤Ã¨Ã Ã­Ã»
 	p[0].x = p2[0].x = 0.0;
 	p[0].y = p2[0].y = 0.0;
 	p[0].z = 0.0;
@@ -157,12 +158,12 @@ four4angel::four4angel(double _s1, double _s2, double _s3, double _s4, double _u
 	p[3].y = p2[3].y = _s4 * round(sin(_u));
 	p[3].z = 0.0;
 	p2[3].z = h;
-	double Diagonal_naprotiv_ugla = sqrt(pow(_s4, 2) + pow(_s1, 2) - 2 * _s1 * _s4 * cos(_u)); // ïî òåîðåìå êîñèíóñîâ
-	double Ugol_4_1 = acos((pow(_s4, 2) - pow(_s1, 2) - pow(Diagonal_naprotiv_ugla, 2)) / ((-2) * Diagonal_naprotiv_ugla * _s1)); // óãîë ïîä äèàãîíàëüþ ñâåðõó
-	double Ugol_4_2 = acos((pow(_s3, 2) - pow(_s2, 2) - pow(Diagonal_naprotiv_ugla, 2)) / ((-2) * _s2 * Diagonal_naprotiv_ugla)); // óãîë íàä äèàãîíàëüþ ñâåðõó
+	double Diagonal_naprotiv_ugla = sqrt(pow(_s4, 2) + pow(_s1, 2) - 2 * _s1 * _s4 * cos(_u)); // Ã¯Ã® Ã²Ã¥Ã®Ã°Ã¥Ã¬Ã¥ ÃªÃ®Ã±Ã¨Ã­Ã³Ã±Ã®Ã¢
+	double Ugol_4_1 = acos((pow(_s4, 2) - pow(_s1, 2) - pow(Diagonal_naprotiv_ugla, 2)) / ((-2) * Diagonal_naprotiv_ugla * _s1)); // Ã³Ã£Ã®Ã« Ã¯Ã®Ã¤ Ã¤Ã¨Ã Ã£Ã®Ã­Ã Ã«Ã¼Ã¾ Ã±Ã¢Ã¥Ã°ÃµÃ³
+	double Ugol_4_2 = acos((pow(_s3, 2) - pow(_s2, 2) - pow(Diagonal_naprotiv_ugla, 2)) / ((-2) * _s2 * Diagonal_naprotiv_ugla)); // Ã³Ã£Ã®Ã« Ã­Ã Ã¤ Ã¤Ã¨Ã Ã£Ã®Ã­Ã Ã«Ã¼Ã¾ Ã±Ã¢Ã¥Ã°ÃµÃ³
 	double Ugol_4 = Ugol_4_1 + Ugol_4_2;
-	double Diagonal_iz_ugla = sqrt(pow(_s1, 2) + pow(_s2, 2) - 2 * _s1 * _s2 * cos(Ugol_4)); // ïî òåîðåìå êîñèíóñîâ
-	double Ugol_1_1 = asin((_s2 * sin(Ugol_4)) / (Diagonal_iz_ugla)); // ÷àñòü èçâåñòíîãî óãëà ïîä äèàãîíàëüþ, âûõîäÿùåé èç íåãî
+	double Diagonal_iz_ugla = sqrt(pow(_s1, 2) + pow(_s2, 2) - 2 * _s1 * _s2 * cos(Ugol_4)); // Ã¯Ã® Ã²Ã¥Ã®Ã°Ã¥Ã¬Ã¥ ÃªÃ®Ã±Ã¨Ã­Ã³Ã±Ã®Ã¢
+	double Ugol_1_1 = asin((_s2 * sin(Ugol_4)) / (Diagonal_iz_ugla)); // Ã·Ã Ã±Ã²Ã¼ Ã¨Ã§Ã¢Ã¥Ã±Ã²Ã­Ã®Ã£Ã® Ã³Ã£Ã«Ã  Ã¯Ã®Ã¤ Ã¤Ã¨Ã Ã£Ã®Ã­Ã Ã«Ã¼Ã¾, Ã¢Ã»ÃµÃ®Ã¤Ã¿Ã¹Ã¥Ã© Ã¨Ã§ Ã­Ã¥Ã£Ã®
 
 	p[2].x = p2[2].x = Diagonal_iz_ugla * cos(Ugol_1_1);
 	p[2].y = p2[2].y = Diagonal_iz_ugla * sin(Ugol_1_1);
@@ -171,66 +172,66 @@ four4angel::four4angel(double _s1, double _s2, double _s3, double _s4, double _u
 
 	calcVectors3d();
 }
-double scalVect(point v1, point v2) // ñêàëÿðíîå ïðîèçâåäåíèå âåêòîðîâ
+double scalVect(point v1, point v2) // Ã±ÃªÃ Ã«Ã¿Ã°Ã­Ã®Ã¥ Ã¯Ã°Ã®Ã¨Ã§Ã¢Ã¥Ã¤Ã¥Ã­Ã¨Ã¥ Ã¢Ã¥ÃªÃ²Ã®Ã°Ã®Ã¢
 {
 	return v1.x * v2.x + v1.y * v2.y;
 };
-double ugolvect(point v1, point v2) // íàõîæäåíèå óãëà â ðàäèàíàõ ìåæäó äâóìÿ âåêòîðàìè
+double ugolvect(point v1, point v2) // Ã­Ã ÃµÃ®Ã¦Ã¤Ã¥Ã­Ã¨Ã¥ Ã³Ã£Ã«Ã  Ã¢ Ã°Ã Ã¤Ã¨Ã Ã­Ã Ãµ Ã¬Ã¥Ã¦Ã¤Ã³ Ã¤Ã¢Ã³Ã¬Ã¿ Ã¢Ã¥ÃªÃ²Ã®Ã°Ã Ã¬Ã¨
 {
 	return acos(scalVect(v1, v2) / sqrt(scalVect(v1, v1) * scalVect(v2, v2)));
 };
-void four4angel::PrintCord(int i) // âûâîä êîîðäèíàò òî÷åê
+void four4angel::PrintCord(int i) // Ã¢Ã»Ã¢Ã®Ã¤ ÃªÃ®Ã®Ã°Ã¤Ã¨Ã­Ã Ã² Ã²Ã®Ã·Ã¥Ãª
 {
 	if (i-1 >= 4||i-1<0)
-		cout << "Òàêîé òî÷êè íåò"<< endl;
+		cout << "Ã’Ã ÃªÃ®Ã© Ã²Ã®Ã·ÃªÃ¨ Ã­Ã¥Ã²"<< endl;
 	else
-		cout << "Êîîðäèíàòû " << i << " òî÷êè " << "  " << p[i-1].x << '\t' << p[i-1].y << endl;
+		cout << "ÃŠÃ®Ã®Ã°Ã¤Ã¨Ã­Ã Ã²Ã» " << i << " Ã²Ã®Ã·ÃªÃ¨ " << "  " << p[i-1].x << '\t' << p[i-1].y << endl;
 }
-void four4angel::PrintCord3d(int i) // âûâîä êîîðäèíàò òî÷åê
+void four4angel::PrintCord3d(int i) // Ã¢Ã»Ã¢Ã®Ã¤ ÃªÃ®Ã®Ã°Ã¤Ã¨Ã­Ã Ã² Ã²Ã®Ã·Ã¥Ãª
 {
 	if (i - 1 >= 8 || i - 1 < 0)
-		cout << "Òàêîé òî÷êè íåò" << endl;
+		cout << "Ã’Ã ÃªÃ®Ã© Ã²Ã®Ã·ÃªÃ¨ Ã­Ã¥Ã²" << endl;
 	else
 		if (i <= 4)
-			cout << "Êîîðäèíàòû " << i << " òî÷êè " << "  " << p[i - 1].x << '\t' << p[i - 1].y << '\t' << p[i - 1].z << endl;
+			cout << "ÃŠÃ®Ã®Ã°Ã¤Ã¨Ã­Ã Ã²Ã» " << i << " Ã²Ã®Ã·ÃªÃ¨ " << "  " << p[i - 1].x << '\t' << p[i - 1].y << '\t' << p[i - 1].z << endl;
 		else
-			cout << "Êîîðäèíàòû " << i << " òî÷êè " << "  " << p2[i - 5].x << '\t' << p2[i - 5].y << '\t' << p2[i - 5].z << endl;
+			cout << "ÃŠÃ®Ã®Ã°Ã¤Ã¨Ã­Ã Ã²Ã» " << i << " Ã²Ã®Ã·ÃªÃ¨ " << "  " << p2[i - 5].x << '\t' << p2[i - 5].y << '\t' << p2[i - 5].z << endl;
 }
-void four4angel::PrintDlin(int i) // âûâîä äëèí ñòîðîí
+void four4angel::PrintDlin(int i) // Ã¢Ã»Ã¢Ã®Ã¤ Ã¤Ã«Ã¨Ã­ Ã±Ã²Ã®Ã°Ã®Ã­
 {
 	if (i - 1 >= 4 || i - 1 < 0)
-		cout << "Òàêîé ñòîðîíû íåò";
+		cout << "Ã’Ã ÃªÃ®Ã© Ã±Ã²Ã®Ã°Ã®Ã­Ã» Ã­Ã¥Ã²";
 	else
-		cout << "Äëèíà "<< i << " ñòîðîíû ðàâíà "<< mv[i-1];
+		cout << "Ã„Ã«Ã¨Ã­Ã  "<< i << " Ã±Ã²Ã®Ã°Ã®Ã­Ã» Ã°Ã Ã¢Ã­Ã  "<< mv[i-1];
 }
-void four4angel::PrintDlin3d(int i) // âûâîä äëèí ñòîðîí
+void four4angel::PrintDlin3d(int i) // Ã¢Ã»Ã¢Ã®Ã¤ Ã¤Ã«Ã¨Ã­ Ã±Ã²Ã®Ã°Ã®Ã­
 {
 	if (i - 1 >= 12 || i - 1 < 0)
-		cout << "Òàêîé ñòîðîíû íåò";
+		cout << "Ã’Ã ÃªÃ®Ã© Ã±Ã²Ã®Ã°Ã®Ã­Ã» Ã­Ã¥Ã²";
 	else
 		if(i<=4)
-			cout << "Äëèíà " << i << " ñòîðîíû ðàâíà " << mv[i - 1];
+			cout << "Ã„Ã«Ã¨Ã­Ã  " << i << " Ã±Ã²Ã®Ã°Ã®Ã­Ã» Ã°Ã Ã¢Ã­Ã  " << mv[i - 1];
 	    if (i>4 && i<=8 )
-		    cout << "Äëèíà " << i << " ñòîðîíû ðàâíà " << h;
+		    cout << "Ã„Ã«Ã¨Ã­Ã  " << i << " Ã±Ã²Ã®Ã°Ã®Ã­Ã» Ã°Ã Ã¢Ã­Ã  " << h;
 		if (i>8)
-			cout << "Äëèíà " << i << " ñòîðîíû ðàâíà " << mv[i - 9];
+			cout << "Ã„Ã«Ã¨Ã­Ã  " << i << " Ã±Ã²Ã®Ã°Ã®Ã­Ã» Ã°Ã Ã¢Ã­Ã  " << mv[i - 9];
 }
-void four4angel::Diagonali() // âûâîä äëèí äèàãîíàëåé
+void four4angel::Diagonali() // Ã¢Ã»Ã¢Ã®Ã¤ Ã¤Ã«Ã¨Ã­ Ã¤Ã¨Ã Ã£Ã®Ã­Ã Ã«Ã¥Ã©
 {
 	d11 = sqrt(pow((p[0].x - p[2].x), 2) + pow((p[0].y - p[2].y), 2));
 	d22 = sqrt(pow((p[1].x - p[3].x), 2) + pow((p[1].y - p[3].y), 2));
 	cout << d11 << '\t' << d22;
 }
-void four4angel::Diagonali3d() // âûâîä äëèí äèàãîíàëåé
+void four4angel::Diagonali3d() // Ã¢Ã»Ã¢Ã®Ã¤ Ã¤Ã«Ã¨Ã­ Ã¤Ã¨Ã Ã£Ã®Ã­Ã Ã«Ã¥Ã©
 {
 	d11 = sqrt(pow((p[0].x - p[2].x), 2) + pow((p[0].y - p[2].y), 2));
 	d22 = sqrt(pow((p[1].x - p[3].x), 2) + pow((p[1].y - p[3].y), 2));
 	d3d1 = sqrt(pow(d11, 2) + pow(h, 2));
 	d3d2 = sqrt(pow(d22, 2) + pow(h, 2));
-	cout << "Äèàãîíàëè îñíîâàíèé ðàâíû " << d11 << '\t' << d22<< endl;
-	cout << "Äëèíû äèàãîíàëåé ïðèçìû ðàâíû " << d3d1 << '\t' << d3d2;
+	cout << "Ã„Ã¨Ã Ã£Ã®Ã­Ã Ã«Ã¨ Ã®Ã±Ã­Ã®Ã¢Ã Ã­Ã¨Ã© Ã°Ã Ã¢Ã­Ã» " << d11 << '\t' << d22<< endl;
+	cout << "Ã„Ã«Ã¨Ã­Ã» Ã¤Ã¨Ã Ã£Ã®Ã­Ã Ã«Ã¥Ã© Ã¯Ã°Ã¨Ã§Ã¬Ã» Ã°Ã Ã¢Ã­Ã» " << d3d1 << '\t' << d3d2;
 }
-double four4angel::Perimetr() // âû÷èñëåíèå ïåðèìåòðà
+double four4angel::Perimetr() // Ã¢Ã»Ã·Ã¨Ã±Ã«Ã¥Ã­Ã¨Ã¥ Ã¯Ã¥Ã°Ã¨Ã¬Ã¥Ã²Ã°Ã 
 {
 	for (int i = 0; i < NUM_POINTS;i++)
 	{
@@ -239,7 +240,7 @@ double four4angel::Perimetr() // âû÷èñëåíèå ïåðèìåòðà
 	}
 	return Per;
 }
-double four4angel::Perimetr3d() // âû÷èñëåíèå ïåðèìåòðà
+double four4angel::Perimetr3d() // Ã¢Ã»Ã·Ã¨Ã±Ã«Ã¥Ã­Ã¨Ã¥ Ã¯Ã¥Ã°Ã¨Ã¬Ã¥Ã²Ã°Ã 
 {
 	for (int i = 0; i < NUM_POINTS;i++)
 	{
@@ -248,12 +249,12 @@ double four4angel::Perimetr3d() // âû÷èñëåíèå ïåðèìåòðà
 	}
 	return Per*2 + 4*h;
 }
-double four4angel::Ploshad() // âû÷èñëåíèå ïëîùàäè
+double four4angel::Ploshad() // Ã¢Ã»Ã·Ã¨Ã±Ã«Ã¥Ã­Ã¨Ã¥ Ã¯Ã«Ã®Ã¹Ã Ã¤Ã¨
 {
 	S=d11*d22*sin(ugolvect(d1,d2))/2;
 	return S;
 }
-double four4angel::Ploshad3d() // âû÷èñëåíèå ïëîùàäè ïîëíîé ïîâåðõíîñòè
+double four4angel::Ploshad3d() // Ã¢Ã»Ã·Ã¨Ã±Ã«Ã¥Ã­Ã¨Ã¥ Ã¯Ã«Ã®Ã¹Ã Ã¤Ã¨ Ã¯Ã®Ã«Ã­Ã®Ã© Ã¯Ã®Ã¢Ã¥Ã°ÃµÃ­Ã®Ã±Ã²Ã¨
 {
 	return d11 * d22 * sin(ugolvect(d1, d2)) + h * mv[0] + h * mv[1] + h * mv[2] + h * mv[3];
 }
@@ -261,7 +262,7 @@ double four4angel::Volume()
 {
 	return (d11 * d22 * sin(ugolvect(d1, d2)) / 2) * h;
 }
-double four4angel::PrintUgol(int i) // âûâîä óãëà ïî íîìåðó âåðøèíû
+double four4angel::PrintUgol(int i) // Ã¢Ã»Ã¢Ã®Ã¤ Ã³Ã£Ã«Ã  Ã¯Ã® Ã­Ã®Ã¬Ã¥Ã°Ã³ Ã¢Ã¥Ã°Ã¸Ã¨Ã­Ã»
 {
 	ugol[0] = Pi - ugolvect(v[0], v[3]);
 	ugol[1] = Pi - ugolvect(v[0], v[1]);
@@ -274,23 +275,23 @@ double four4angel::PrintUgol(int i) // âûâîä óãëà ïî íîìåðó âåðøèíû
 			ugol[i] = 2 * Pi - ugol[i];
 	}
 	if (i - 1 >= 4 || i - 1 < 0)
-		cout << "Òàêîé âåðøèíû íåò";
+		cout << "Ã’Ã ÃªÃ®Ã© Ã¢Ã¥Ã°Ã¸Ã¨Ã­Ã» Ã­Ã¥Ã²";
 	else
-		cout << "Óãîë ïðè " << i << " âåðøèíå ðàâåí " << ugol[i - 1] << " ðàäèàí èëè æå ïðèáëèçèòåëüíî " << round(ugol[i - 1] * 180 / Pi) << " ãðàäóñîâ " << endl;
+		cout << "Ã“Ã£Ã®Ã« Ã¯Ã°Ã¨ " << i << " Ã¢Ã¥Ã°Ã¸Ã¨Ã­Ã¥ Ã°Ã Ã¢Ã¥Ã­ " << ugol[i - 1] << " Ã°Ã Ã¤Ã¨Ã Ã­ Ã¨Ã«Ã¨ Ã¦Ã¥ Ã¯Ã°Ã¨Ã¡Ã«Ã¨Ã§Ã¨Ã²Ã¥Ã«Ã¼Ã­Ã® " << round(ugol[i - 1] * 180 / Pi) << " Ã£Ã°Ã Ã¤Ã³Ã±Ã®Ã¢ " << endl;
 	return ugol[i - 1];
 }
-double four4angel::PrintUgol(int i, int a) // âûâîä óãëà ïî ñòîðîíàì ìåæäó êîòîðûìè îí ëåæèò
+double four4angel::PrintUgol(int i, int a) // Ã¢Ã»Ã¢Ã®Ã¤ Ã³Ã£Ã«Ã  Ã¯Ã® Ã±Ã²Ã®Ã°Ã®Ã­Ã Ã¬ Ã¬Ã¥Ã¦Ã¤Ã³ ÃªÃ®Ã²Ã®Ã°Ã»Ã¬Ã¨ Ã®Ã­ Ã«Ã¥Ã¦Ã¨Ã²
 {
 	double r = Pi - ugolvect(v[i - 1], v[a - 1]);
 	if (r > Pi / 2)
 		r = 2 * Pi - r;
 	if (i - 1 >= 4 || i - 1 < 0 || -1 >= 4 || a - 1 < 0)
-		cout << "Òàêîé ñòîðîíû íåò" << endl;
+		cout << "Ã’Ã ÃªÃ®Ã© Ã±Ã²Ã®Ã°Ã®Ã­Ã» Ã­Ã¥Ã²" << endl;
 	else
-		cout << "Óãîë ìåæäó " << i << " è " << a << " ñòîðîíàìè ðàâåí  " << r << " ðàäèàí èëè æå ïðèáëèçèòåëüíî " << round(r * 180 / Pi) << " ãðàäóñîâ " << endl;
+		cout << "Ã“Ã£Ã®Ã« Ã¬Ã¥Ã¦Ã¤Ã³ " << i << " Ã¨ " << a << " Ã±Ã²Ã®Ã°Ã®Ã­Ã Ã¬Ã¨ Ã°Ã Ã¢Ã¥Ã­  " << r << " Ã°Ã Ã¤Ã¨Ã Ã­ Ã¨Ã«Ã¨ Ã¦Ã¥ Ã¯Ã°Ã¨Ã¡Ã«Ã¨Ã§Ã¨Ã²Ã¥Ã«Ã¼Ã­Ã® " << round(r * 180 / Pi) << " Ã£Ã°Ã Ã¤Ã³Ã±Ã®Ã¢ " << endl;
 	return r;
 }
-void four4angel::Vogn() // ïðîâåðêà íà âîãíóòîñòü/âûïóêëîñòü
+void four4angel::Vogn() // Ã¯Ã°Ã®Ã¢Ã¥Ã°ÃªÃ  Ã­Ã  Ã¢Ã®Ã£Ã­Ã³Ã²Ã®Ã±Ã²Ã¼/Ã¢Ã»Ã¯Ã³ÃªÃ«Ã®Ã±Ã²Ã¼
 {
 	d1.x = p[2].x - p[0].x;
 	d1.y = p[2].y - p[0].y;
@@ -301,11 +302,11 @@ void four4angel::Vogn() // ïðîâåðêà íà âîãíóòîñòü/âûïóêëîñòü
 	double v3 = d1.x * (p[3].y - p[0].y) - d1.y * (p[3].x - p[0].x);
 	double v4 = d1.x * v[0].y - d1.y * v[0].x;
 	if (v1 * v2 < 0 && v4 * v3 < 0)
-		cout << "×åòûð¸õóãîëüíèê âûïóêëûé";
+		cout << "Ã—Ã¥Ã²Ã»Ã°Â¸ÃµÃ³Ã£Ã®Ã«Ã¼Ã­Ã¨Ãª Ã¢Ã»Ã¯Ã³ÃªÃ«Ã»Ã©";
 	else
-		cout << "×åòûð¸õóãîëüíèê âîãíóòûé";
+		cout << "Ã—Ã¥Ã²Ã»Ã°Â¸ÃµÃ³Ã£Ã®Ã«Ã¼Ã­Ã¨Ãª Ã¢Ã®Ã£Ã­Ã³Ã²Ã»Ã©";
 }
-void four4angel::Vogn3d() // ïðîâåðêà íà âîãíóòîñòü/âûïóêëîñòü
+void four4angel::Vogn3d() // Ã¯Ã°Ã®Ã¢Ã¥Ã°ÃªÃ  Ã­Ã  Ã¢Ã®Ã£Ã­Ã³Ã²Ã®Ã±Ã²Ã¼/Ã¢Ã»Ã¯Ã³ÃªÃ«Ã®Ã±Ã²Ã¼
 {
 	d1.x = p[2].x - p[0].x;
 	d1.y = p[2].y - p[0].y;
@@ -316,12 +317,12 @@ void four4angel::Vogn3d() // ïðîâåðêà íà âîãíóòîñòü/âûïóêëîñòü
 	double v3 = d1.x * (p[3].y - p[0].y) - d1.y * (p[3].x - p[0].x);
 	double v4 = d1.x * v[0].y - d1.y * v[0].x;
 	if (v1 * v2 < 0 && v4 * v3 < 0)
-		cout << "Îñíîâàíèå ïðèçìû âûïóêëîå";
+		cout << "ÃŽÃ±Ã­Ã®Ã¢Ã Ã­Ã¨Ã¥ Ã¯Ã°Ã¨Ã§Ã¬Ã» Ã¢Ã»Ã¯Ã³ÃªÃ«Ã®Ã¥";
 	else
-		cout << "Îñíîâàíèå ïðèçìû âîãíóòîå";
-		cout << "Ýòî ÿ äîáàâèëà äëÿ ãèòà";
+		cout << "ÃŽÃ±Ã­Ã®Ã¢Ã Ã­Ã¨Ã¥ Ã¯Ã°Ã¨Ã§Ã¬Ã» Ã¢Ã®Ã£Ã­Ã³Ã²Ã®Ã¥";
+		cout << "ÃÃ²Ã® Ã¿ Ã¤Ã®Ã¡Ã Ã¢Ã¨Ã«Ã  Ã¤Ã«Ã¿ Ã£Ã¨Ã²Ã ";
 }
-four4angel::~four4angel(void) // äåñòðóêòîð
+four4angel::~four4angel(void) // Ã¤Ã¥Ã±Ã²Ã°Ã³ÃªÃ²Ã®Ã°
 {
 
 }
